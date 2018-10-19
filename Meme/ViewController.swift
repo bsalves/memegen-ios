a1b733e8b9c0b2f776ada74b2f3743c05d7ae473
Bruno Alves <oemaildobrunoalves@gmail.com>
2018-10-19 00:22:54 -0300

    Basic meme image shot, take from camera roll and share

--- Meme/ViewController.swift
+++ Meme/ViewController.swift
@@ -10,16 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottonText: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var topBar: UIToolbar!
+    @IBOutlet weak var bottomBar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func generateMemedImage() -> UIImage {
+        
+        hideBars()
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        showBars()
+        
+        return memedImage
     }
-
-
+    
+    private func hideBars() {
+        topBar.isHidden = true
+        bottomBar.isHidden = true
+    }
+    
+    private func showBars() {
+        topBar.isHidden = false
+        bottomBar.isHidden = false
+    }
+    
+    @IBAction func shot(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let imagePicker =  UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Erro!", message: "Não foi possível iniciar a camera do seu dispositivo.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func takeFromAlbum(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        
+        // set up activity view controller
+        let imageToShare = [ generateMemedImage() ]
+        let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        
+        // exclude some activity types from the list (optional)
+        activityViewController.excludedActivityTypes = [ UIActivityType.airDrop, UIActivityType.postToFacebook ]
+        
+        // present the view controller
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        image.image = nil
+        topText.text = nil
+        bottonText.text = nil
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
-
